7c9154b9d888ad5f41fcea4a86e8466a714a3ab0
Belicio Batista Cardoso <belloinfo@gmail.com>
2023-05-28 17:17:48 -0300

    fix line

--- weather/src/main/java/com/example/weather/service/OpenWeatherEndpoint.java
+++ weather/src/main/java/com/example/weather/service/OpenWeatherEndpoint.java
@@ -1,7 +1,6 @@
 package com.example.weather.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
